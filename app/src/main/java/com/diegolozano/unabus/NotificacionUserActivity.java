db0a37cb35a3389313ffb4c52498c47e06db30ea
diegolozano <dlozano245@unab.edu.co>
2023-11-01 16:16:54 -0500

    1/11/2023

--- app/src/main/java/com/diegolozano/unabus/NotificacionUserActivity.java
+++ app/src/main/java/com/diegolozano/unabus/NotificacionUserActivity.java
@@ -13,11 +13,24 @@ public class NotificacionUserActivity extends AppCompatActivity {
     NotificacionAdapter myAdapter;
     RecyclerView rvNotificaciones;
 
+
+    private String[] localDataSetparada = new String[3];
+    NotificacionAdapter myAdapterparada;
+    RecyclerView rvParada;
+
+    private String[] localDataSetruta = new String[3];
+    NotificacionAdapter myAdapterruta;
+    RecyclerView rvRuta;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificacion_user);
         cargarfackedata();
+        cargarfackedata1();
+        cargarfackedata2();
         myAdapter = new NotificacionAdapter(localDataSet);
         rvNotificaciones = findViewById(R.id.rv_notificaciones);
 
@@ -25,6 +38,17 @@ public class NotificacionUserActivity extends AppCompatActivity {
         rvNotificaciones.setLayoutManager(new GridLayoutManager(this,2));
 
 
+        myAdapterparada = new NotificacionAdapter(localDataSetparada);
+        rvParada = findViewById(R.id.rv_parada);
+
+        rvParada.setAdapter(myAdapterparada);
+        rvParada.setLayoutManager(new GridLayoutManager(this,2));
+
+        myAdapterruta = new NotificacionAdapter(localDataSetruta);
+        rvRuta = findViewById(R.id.rv_ruta);
+
+        rvRuta.setAdapter(myAdapterruta);
+        rvRuta.setLayoutManager(new GridLayoutManager(this,2));
 
 
     }
@@ -33,5 +57,18 @@ public class NotificacionUserActivity extends AppCompatActivity {
         localDataSet[0]="Notificacion 1";
         localDataSet[1]="Notificacion 2";
         localDataSet[2]="Notificacion 3";
+    }
+
+
+    private void cargarfackedata1() {
+        localDataSetparada[0]="Parada 1";
+        localDataSetparada[1]="Parada 2";
+        localDataSetparada[2]="Parada 3";
+    }
+
+    private void cargarfackedata2() {
+        localDataSetruta[0]="ruta1";
+        localDataSetruta[1]="ruta2";
+        localDataSetruta[2]="ruta3";
     }
 }
